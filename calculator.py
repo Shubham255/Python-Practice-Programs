d100feeb70f1859d31f52f6e7272977d2da388b8
Aniket Sunil Pawar <54832294+aniketpawar9211@users.noreply.github.com>
2020-10-24 11:47:59 +0530

    Using Operator

--- calculator.py
+++ calculator.py
@@ -1,6 +1,26 @@
-while True:
+from operator import pow, truediv, mul, add, sub  
+
+operators = {
+  '+': add,
+  '-': sub,
+  '*': mul,
+  '/': truediv
+}
+
+def calculate(s):
+    if s.isdigit():
+        return float(s)
+    for c in operators.keys():
+        left, operator, right = s.partition(c)
+        if operator in operators:
+            return operators[operator](calculate(left), calculate(right))
+
+calc = input("Type calculation:\n")
+print("Answer: " + str(calculate(calc)))
+
+'''while True:
     print("*********Calculator**********")
-    print("+ for add")
+    print("+ for add")'
     print("- for sub")
     print("* for mul")
     print("/ for div")
@@ -43,4 +63,5 @@ while True:
         break
     else:
         print("Try again later")
-    
+    '''
+
